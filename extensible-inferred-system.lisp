088d7c01dfe8ac6a5f39c673275384f6e08a3e26
pve1 <pve1@foo.invalid>
2024-02-27 15:45:54 +0200

    Requires-system works.

--- extensible-inferred-system.lisp
+++ extensible-inferred-system.lisp
@@ -18,9 +18,10 @@
   ())
 
 (defgeneric dependency-form-p (primary-system form)
-  (:documentation "Returns non-nil if FORM specifies dependencies."))
+  (:documentation "Returns non-nil if FORM specifies dependencies in the context of
+PRIMARY-SYSTEM."))
 
-(defgeneric extract-dependencies (primary-system dependency-form)
+(defgeneric extract-dependencies (primary-system dependency-form &key pathname)
   (:documentation "Returns dependencies specified by DEPENDENCY-FORM."))
 
 (defgeneric read-dependencies (primary-system file)
@@ -29,23 +30,40 @@
                   (loop :for form = (read stream nil nil) :while form
                         :when (dependency-form-p primary-system form)
                         :return form))))
-      (extract-dependencies primary-system form)))
+      (extract-dependencies primary-system form :pathname file)))
   (:documentation "Searches FILE for a form satisfying DEPENDENCY-FORM-P. Returns
 dependencies specified by the first form found."))
+
+(defgeneric discover-system (primary-system full-sub-system-name)
+  (:documentation "Returns information about the sub-system FULL-SUB-SYSTEM-NAME or NIL
+if no system can be found."))
+
+(defgeneric maybe-use-existing-system (primary-system discovery)
+  (:documentation "Returns a previously registered asdf system that matches the
+information in DISCOVERY, if one exists."))
+
+(defgeneric generate-fresh-sub-system (primary-system discovery)
+  (:documentation "Generates a new asdf:system instance that corresponds to DISCOVERY."))
 
 (defgeneric infer-sub-system (primary-system full-sub-system-name)
   (:method (primary-system full-sub-system-name)
     nil)
-  (:documentation "Given FULL-SUB-SYSTEM-NAME (like \"my-library/foo\"), generates an
-asdf system instance that corresponds to that name if a suitable
-source file can be found. Returns NIL otherwise."))
+  (:method ((primary-system system) full-sub-system-name)
+    (let ((discovery (discover-system primary-system
+                                      full-sub-system-name)))
+      (when discovery
+        (or (maybe-use-existing-system primary-system discovery)
+            (generate-fresh-sub-system primary-system discovery)))))
+  (:documentation "Given FULL-SUB-SYSTEM-NAME (like \"my-library/foo\"), returns an asdf
+system instance that corresponds to that name if a suitable source
+file can be found. Returns NIL otherwise."))
 
 (defun sysdef-system-search (system-name)
   ;; If system-name is "my-library/foo", then the primary name is
   ;; "my-library".
   (let ((primary-name (asdf:primary-system-name system-name)))
     ;; Don't do anything if system-name wasn't of the form
-    ;; "my-library/foo".
+    ;; "my-library/foo", i.e. not a primary name.
     (unless (equal primary-name system-name)
       ;; Default method returns NIL.
       (infer-sub-system (asdf:find-system primary-name) system-name))))
--- requires-system.lisp
+++ requires-system.lisp
@@ -15,15 +15,10 @@
   ())
 
 (defmethod extract-dependencies ((primary-system requires-system)
-                                 dependency-form)
-   (loop :for dep :in (rest dependency-form)
-        :collect (asdf:coerce-name
-                  (etypecase dep
-                    (string (concatenate 'string
-                                         (asdf:component-name primary-system)
-                                         "/"
-                                         dep))
-                    (symbol dep)))))
+                                 dependency-form
+                                 &key pathname)
+  (declare (ignore pathname))
+  (rest dependency-form))
 
 (defmethod dependency-form-p ((primary-system requires-system) form &aux car)
   (and (listp form)
@@ -31,48 +26,22 @@
        (symbolp car)
        (string-equal car "REQUIRES")))
 
-(defgeneric maybe-use-existing-system (existing-system candidate-properties)
-  (:method (existing-system candidate-properties)
-    nil)
-  (:documentation "Compares EXISTING-SYSTEM to CANDIDATE-PROPERTIES. If the properties
-match those in the system, return EXISTING-SYSTEM, otherwise NIL. The
-purpose of this function is to avoid having to generate a new system
-instance when doing ASDF:FIND-SYSTEM if a previously registered system
-can be used."))
-
-(defmethod maybe-use-existing-system ((existing-system requires-system)
-                                      candidate-properties)
-  (destructuring-bind (&key system-directory
-                            dependencies
-                            full-sub-system-name
-                            sub-system-file) candidate-properties
-    ;; Should we check around-compile-hook?
-    (and (eq (type-of existing-system) 'requires-system)
-         (equal (asdf:component-name existing-system)
-                full-sub-system-name)
-         (uiop:pathname-equal
-          system-directory
-          (asdf:component-pathname existing-system))
-         (equal dependencies (asdf:component-sideway-dependencies
-                              existing-system))
-         ;; Single child of type cl-source-file?
-         (let (children child)
-           (and (setf children (asdf:component-children existing-system))
-                (setf child (first children))
-                (null (cdr children))
-                (eq (type-of child) 'asdf:cl-source-file)
-                (uiop:pathname-equal sub-system-file
-                                     (asdf:component-pathname
-                                      child))))
-         existing-system)))
-
 (defclass system-discovery ()
   ((full-sub-system-name :initarg :full-sub-system-name
                          :accessor full-sub-system-name
                          :initform nil)
+   (sub-system-name :initarg :sub-system-name
+                    :accessor sub-system-name
+                    :initform nil)
+   (relative-path :initarg :relative-path
+                  :accessor relative-path
+                  :initform nil)
    (system-directory :initarg :system-directory
                      :accessor system-directory
                      :initform nil)
+   (dependencies :initarg :dependencies
+                 :accessor dependencies
+                 :initform nil)
    (component-type :initarg :component-type
                    :accessor component-type
                    :initform nil)
@@ -86,78 +55,54 @@ can be used."))
                          :accessor sub-system-directory
                          :initform nil)))
 
-(defgeneric discover-system (primary-system full-sub-system-name)
-  (:method ((primary-system requires-system) full-sub-system-name)
-    (let* ((sub-system-name (subseq full-sub-system-name
-                                    (1+ (length (asdf:component-name
-                                                 primary-system)))))
-           (system-directory (asdf:component-pathname primary-system))
-           (component-type (class-name
-                            (asdf/defsystem:class-for-type primary-system
-                                                           :file)))
-           (file-type (asdf:file-type (make-instance component-type)))
-           (sub-system-name-ends-in-slash ; Indicates directory.
-             (eql #\/ (uiop:last-char sub-system-name)))
-           (sub-system-file
-             (uiop:file-exists-p
-              (uiop:subpathname system-directory
-                                sub-system-name
-                                :type file-type)))
-           (sub-system-directory
-             (uiop:directory-exists-p
-              (uiop:subpathname system-directory sub-system-name))))
-
-      ;; Heuristic to determine sub-system-file.
-      ;; Simple case: "/my-lib/foo.lisp" was found.
-      (cond ((and (not sub-system-name-ends-in-slash)
-                  sub-system-file)
-             t)
-
-            ;; Sub system is a directory. Expand like so:
-            ;; "/my-lib/foo" -> "/my-lib/foo/foo.lisp"
-            ((and (or (not sub-system-file)
-                      sub-system-name-ends-in-slash)
-                  sub-system-directory)
-             (let ((last-directory ; the "foo" part
-                     (car (last (pathname-directory
-                                 sub-system-directory)))))
-               (setf sub-system-file
-                     (uiop:subpathname
-                      system-directory
-                      (concatenate 'string
-                                   sub-system-name
-                                   (if sub-system-name-ends-in-slash
-                                       ""
-                                       "/")
-                                   last-directory)
-                      :type file-type)))))
-      (when sub-system-file
-        (make-instance 'system-discovery
-          :full-sub-system-name full-sub-system-name
-          :system-directory system-directory
-          :sub-system-file sub-system-file
-          :sub-system-directory sub-system-directory
-          :file-type file-type
-          :component-type component-type))))
-  (:documentation "Returns a discovery instance containing preliminary information about
-the system specified by FULL-SUB-SYSTEM-NAME. Returns NIL if no
-applicable system is found."))
-
-(defmethod infer-sub-system ((primary-system requires-system) full-sub-system-name)
+(defmethod discover-system ((primary-system requires-system) full-sub-system-name)
   (let* ((sub-system-name (subseq full-sub-system-name
                                   (1+ (length (asdf:component-name
                                                primary-system)))))
+         (relative-path sub-system-name) ; May change below.
          (system-directory (asdf:component-pathname primary-system))
          (component-type (class-name
                           (asdf/defsystem:class-for-type primary-system
                                                          :file)))
          (file-type (asdf:file-type (make-instance component-type)))
+         (sub-system-name-ends-in-slash ; Indicates directory.
+           (eql #\/ (uiop:last-char sub-system-name)))
          (sub-system-file
-           (uiop:probe-file*
+           (uiop:file-exists-p
             (uiop:subpathname system-directory
                               sub-system-name
-                              :type file-type)
-            :truename asdf:*resolve-symlinks*)))
+                              :type file-type)))
+         (sub-system-directory
+           (uiop:directory-exists-p
+            (uiop:subpathname system-directory sub-system-name))))
+
+    ;; Heuristic to determine sub-system-file.
+    ;; Simple case: "/my-lib/foo.lisp" was found.
+    (cond ((and (not sub-system-name-ends-in-slash)
+                sub-system-file)
+           t)
+
+          ;; Sub-system is a directory. Expand like so:
+          ;; "/my-lib/foo/" -> "/my-lib/foo/foo.lisp"
+          ((and (or (not sub-system-file)
+                    sub-system-name-ends-in-slash)
+                sub-system-directory)
+           (let ((last-directory        ; the "foo" part
+                   (car (last (pathname-directory
+                               sub-system-directory)))))
+             (setf relative-path        ; "foo" -> "foo/foo"
+                   (concatenate 'string
+                                sub-system-name
+                                (if sub-system-name-ends-in-slash
+                                    ""
+                                    "/")
+                                last-directory))
+             ;; Update sub-system-file to reflect new location.
+             (setf sub-system-file
+                   (uiop:subpathname
+                    system-directory
+                    relative-path
+                    :type file-type)))))
 
     ;; FULL-SUB-SYSTEM-NAME: "some-library/foo/util"
     ;; SUB-SYSTEM-NAME: "foo/util"
@@ -165,30 +110,67 @@ applicable system is found."))
     ;; SYSTEM-DIRECTORY: "/full/path/to/some-library/"
     ;; FILE-TYPE: "lisp"
 
-    (cond ((uiop:directory-pathname-p sub-system-file))
-          ;; "/foo/bar/"
+    (when (uiop:file-exists-p sub-system-file)
+      (let ((dependencies
+              (loop :for dep :in (read-dependencies primary-system
+                                                    sub-system-file)
+                    :collect
+                       (asdf:coerce-name
+                        (etypecase dep
+                          (string
+                           (concatenate 'string
+                                        (asdf:component-name
+                                         primary-system)
+                                        "/"
+                                        sub-system-name
+                                        (if sub-system-name-ends-in-slash
+                                            ""
+                                            "/")
+                                        dep))
+                          (symbol dep))))))
+        (make-instance 'system-discovery
+          :full-sub-system-name full-sub-system-name
+          :sub-system-name sub-system-name
+          :relative-path relative-path
+          :dependencies dependencies
+          :system-directory system-directory
+          :sub-system-file sub-system-file
+          :sub-system-directory sub-system-directory
+          :file-type file-type
+          :component-type component-type)))))
 
-          ;; "/.../foo/bar.lisp"
-          ((uiop:file-pathname-p sub-system-file)
-           (let* ((dependencies (read-dependencies primary-system
-                                                   sub-system-file))
-                  (existing-system (asdf:registered-system
-                                    full-sub-system-name)))
-             (or (maybe-use-existing-system
-                  existing-system
-                  (list :system-directory system-directory
-                        :dependencies dependencies
-                        :full-sub-system-name full-sub-system-name
-                        :sub-system-file sub-system-file))
-                 (eval
-                  `(asdf:defsystem ,full-sub-system-name
-                     :class ,(class-name (class-of primary-system))
-                     :default-component-class asdf:cl-source-file
-                     :source-file ,(asdf:system-source-file primary-system)
-                     :pathname ,system-directory ;; Primary system
-                     :depends-on ,dependencies
-                     ;; :around-compile ,around-compile
-                     :components ((,component-type
-                                   ,file-type
-                                   :pathname ,sub-system-name))))))))))
+(defmethod maybe-use-existing-system ((existing-system requires-system)
+                                      (discovery system-discovery))
+    ;; Should we check around-compile-hook?
+    (and (eq (type-of existing-system) 'requires-system)
+         (equal (asdf:component-name existing-system)
+                (full-sub-system-name discovery))
+         (uiop:pathname-equal
+          (system-directory discovery)
+          (asdf:component-pathname existing-system))
+         (equal (dependencies discovery)
+                (asdf:component-sideway-dependencies
+                 existing-system))
+         ;; Single child of type cl-source-file?
+         (let (children child)
+           (and (setf children (asdf:component-children existing-system))
+                (setf child (first children))
+                (null (cdr children))
+                (eq (type-of child) 'asdf:cl-source-file)
+                (uiop:pathname-equal (sub-system-file discovery)
+                                     (asdf:component-pathname
+                                      child))))
+         existing-system))
 
+(defmethod generate-fresh-sub-system ((primary-system requires-system) discovery)
+  (eval
+   `(asdf:defsystem ,(full-sub-system-name discovery)
+      :class ,(class-name (class-of primary-system))
+      :default-component-class asdf:cl-source-file
+      :source-file ,(asdf:system-source-file primary-system)
+      :pathname ,(system-directory discovery) ;; Primary system
+      :depends-on ,(dependencies discovery)
+      ;; :around-compile ,around-compile
+      :components ((,(component-type discovery)
+                    ,(file-type discovery)
+                    :pathname ,(relative-path discovery))))))
