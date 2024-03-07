18d54efec9470f0c58b7b9a88cbce01305b0264e
pve1 <pve1@foo.invalid>
2024-03-07 15:05:28 +0200

    Fixed scripts.

--- test/quote/run-tests.lisp
+++ test/quote/run-tests.lisp
@@ -8,9 +8,9 @@
 (defun load-system (system)
   (asdf:load-system system))
 
-(load-system "extensible-inferred-system-test/basic")
-(load-system "extensible-inferred-system-test/basic/basic-full")
-(load-system "extensible-inferred-system-test/full")
+(load-system "extensible-inferred-system-quote-test/basic")
+(load-system "extensible-inferred-system-quote-test/basic/basic-full")
+(load-system "extensible-inferred-system-quote-test/full")
 
 (terpri)
 (uiop:quit)
